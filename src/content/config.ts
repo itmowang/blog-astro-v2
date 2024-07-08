ae13e7f4fa71592b0dae101fcf74472779ab8c18
Mowang <mw@loli.wang>
2024-07-08 13:46:07 +0800

    📃 docs:xf

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,8 @@
-import { defineCollection } from 'astro:content';
-import { rssSchema } from '@astrojs/rss';
+// import { defineCollection } from 'astro:content';
+// import { rssSchema } from '@astrojs/rss';
 
-const blog = defineCollection({
-  schema: rssSchema,
-});
+// const blog = defineCollection({
+//   schema: rssSchema,
+// });
 
-export const collections = { blog };
+// export const collections = { blog };
